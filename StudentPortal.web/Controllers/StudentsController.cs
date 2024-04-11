35ae5025a21c37b374ec6ba871c2ac493899e3f3
Kishankumary <Kishankumary@ICS-LT-48B19K3>
2024-04-11 15:44:20 +0530

    Add project files.

---
+++ StudentPortal.web/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.web.Data;
+using StudentPortal.web.Models;
+using StudentPortal.web.Models.Entites;
+
+namespace StudentPortal.web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDBContext dBContext;
+
+        public StudentsController(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Add(AddStudentViewModel viewModel)
+        {
+            var student =new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed,
+                
+            };
+            await dBContext.students.AddAsync(student);
+            await dBContext.SaveChangesAsync();
+
+            return View();
+        }
+      
+        }
+    }
+
---
+++ StudentPortal.web/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.web.Models.Entites;
+
+namespace StudentPortal.web.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
